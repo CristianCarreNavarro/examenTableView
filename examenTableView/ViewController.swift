63447006cbaa6d0f1ca76ac50d30739df131f1eb
Cristian Carreño Navarro <cristiancarrenonavarro@cristians-imac.local>
2019-03-13 01:44:49 -0700

    Initial Commit

---
+++ examenTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  examenTableView
+//
+//  Created by Cristian Carreño Navarro on 13/03/2019.
+//  Copyright © 2019 Cristian Carreño Navarro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
